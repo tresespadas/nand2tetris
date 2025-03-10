0705d5614b30e01faef3827092e114cfac779910
tresespadas <alvaro8038c@gmail.com>
2025-03-10 09:00:07 +0100

    mult.asm corregido

--- project4/Mult.asm
+++ project4/Mult.asm
@@ -1,33 +1,21 @@
-
-// R2 = R0 * R1
-
-// loop: R0+R0+R0 ... R1 veces
-// for (i=R1;i=0;i--) { sum += R0 }
-//R0: El número que hay que sumar
-//R1: El limite del loop
-//i: El iterador del loop
-//R2: Donde se va haciendo la suma
-
-@6
-D=A
-@R0
-M=D
-@R1
-M=D+1
-@i // Loop desde R1 hasta 0
-M=D+1
-(LOOP) // Empieza el for
-@i // Comprueba si RAM[i]=0 para salir o no
+@R2 // Asegura R2=0
+M=0
+@R1 // Carga el valor de R1
 D=M
-@END
+@i // Itera en base a R1
+M=D
+(LOOP) // Inicio loop
+@i
+D=M
+@END // Comprueba si i=0
 D;JEQ
 @R0
 D=M
-@R2 // R2 = sum
+@R2 // R2=R0+R0... R1 veces
 M=D+M
-@i // RAM[i]--
+@i // Decrece i (i--)
 M=M-1
 @LOOP // Vuelve arriba
 0;JMP
-@END
+@END // Loop de salida
 0;JMP
