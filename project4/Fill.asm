c0470cf190cd509b4b99676f604dd68ef79ec847
tresespadas <alvaro8038c@gmail.com>
2025-03-11 06:46:14 +0100

    avanzando en fill.asm

--- project4/Fill.asm
+++ project4/Fill.asm
@@ -1,6 +1,25 @@
-// loop infinito para leer del teclado
-// dirección del teclado?
+// Prueba: Suma 1 o Resta 1 dependiendo si pulsas el teclado
+@SCREEN
+D=A // Carga la dirección de SCREEN
+@N
+M=D // Guardala en RN
+// if (KBD != 0 ) { M=-1 }; else { M=0 }
+// necesito un loop infinito que lea de KBD
+//while true; do ( if ... )
+(LOOP)
 @KBD
-M;JGT
+D=M // Carga lo que vale KBD
+@SUMA
+D;JGT
+@RESTA
+D;JEQ
+(SUMA)
 @R0
+M=M+1
+@LOOP
+0;JMP
+(RESTA)
+@R0
+M=M-1
+@LOOP
 0;JMP
